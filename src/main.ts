01ff901407d12fbde775a3b2fb34be6d773607ef
RaviAnand Mohabir <moravrav@gmail.com>
2021-08-12 16:11:30 +0200

    ✨ Switch to BS5 Sass for overrides

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/bootstrap.scss";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
