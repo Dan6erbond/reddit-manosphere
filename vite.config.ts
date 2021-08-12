585cd905a75c8b09df295b5c3a9b8508f0e023ef
RaviAnand Mohabir <moravrav@gmail.com>
2021-08-12 18:39:53 +0200

    ✨ Implement SSG with `vite-plugin-ssr`

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import ssr from "vite-plugin-ssr/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), ssr()],
   optimizeDeps: {
     exclude: ["bootstrap"],
   },
